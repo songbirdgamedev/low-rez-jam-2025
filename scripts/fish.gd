19ec60193f7160576a6494b14e9b0d27d26e6ae7
emery <215300895+xexemery@users.noreply.github.com>
2025-08-07 13:39:53 +0100

    Add base for fish class

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -2,6 +2,26 @@ class_name Fish
 extends Node2D
 
 
+@onready var sprite: Sprite2D = $Sprite
+
+const SPRITE_SIZE: int = 16
+const RECT_SIZE: Vector2i = Vector2i(SPRITE_SIZE, SPRITE_SIZE)
+
+var rect_position: Vector2i
+var sprite_region: Rect2i
+var size_inches: float
+
+enum Colour {
+	LIGHTGREY,
+	DARKGREY,
+	BLACK,
+	BROWN,
+	RED,
+	GREEN,
+	SILVER,
+	GOLD
+}
+
 enum Size {
 	S,
 	M,
@@ -9,13 +29,12 @@ enum Size {
 	XL
 }
 
-enum Colour {
-	LIGHTGREY,
-	RED,
-	GREEN,
-	BLACK,
-	DARKGREY,
-	SILVER,
-	GOLD,
-	BROWN
-}
+
+func _init(colour: Colour, size: Size, inches: float) -> void:
+	rect_position = Vector2i(colour * SPRITE_SIZE, size * SPRITE_SIZE)
+	sprite_region = Rect2i(rect_position, RECT_SIZE)
+	size_inches = inches
+
+
+func _ready() -> void:
+	sprite.set_region_rect(sprite_region)
