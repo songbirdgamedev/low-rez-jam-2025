8f4193c06524a110acfcc126ce0882f2476e1765
emery <215300895+xexemery@users.noreply.github.com>
2025-08-12 20:49:31 +0100

    Add message scene and script

---
+++ scripts/message.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+
+@onready var label: Label = $Label
+@onready var timer: Timer = $Label/Timer
+
+
+func _ready() -> void:
+	_hide_text()
+	timer.timeout.connect(_hide_text)
+
+
+func _hide_text() -> void:
+	label.text = ""
+
+
+func show_text(message: String) -> void:
+	label.text = message
+	timer.start()
