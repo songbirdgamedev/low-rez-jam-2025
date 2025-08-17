56fae0717b5bbae18b4054a4116ff6269cd0a39b
emery <215300895+xexemery@users.noreply.github.com>
2025-08-17 16:48:43 +0100

    Add animations to title screen

--- scripts/title.gd
+++ scripts/title.gd
@@ -6,19 +6,52 @@ extends Node2D
 const FILE_PATH: String = "res://scenes/game.tscn"
 
 var animations_finished: bool = false
+var current_line: Line = Line.ZERO
+
+enum Line {
+	ZERO,
+	ONE,
+	TWO,
+	THREE
+}
 
 
 func _ready() -> void:
 	animation_player.animation_finished.connect(_on_animation_finished)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("confirm") and animations_finished:
-		get_tree().call_deferred("change_scene_to_file", FILE_PATH)
+		_advance_to_next()
 
 
 func _on_animation_finished(animation_name: String) -> void:
 	if animation_name == "title":
 		animation_player.play("label")
-	elif animation_name == "label":
+	elif animation_name == "start_game":
+		_start_game()
+	else:
 		animations_finished = true
+
+
+func _advance_to_next() -> void:
+	match current_line:
+		Line.ZERO:
+			animations_finished = false
+			current_line = Line.ONE
+			animation_player.play("fish_controls")
+		Line.ONE:
+			animations_finished = false
+			current_line = Line.TWO
+			animation_player.play("log_controls")
+		Line.TWO:
+			animations_finished = false
+			current_line = Line.THREE
+			animation_player.play("nav_controls")
+		Line.THREE:
+			animations_finished = false
+			animation_player.play("start_game")
+
+
+func _start_game() -> void:
+	get_tree().call_deferred("change_scene_to_file", FILE_PATH)
